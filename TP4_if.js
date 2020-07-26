8b11357fbb6eb15c34f8c82a6780ec893c4b3d7c
damian <damian@casa.com>
2020-07-26 10:49:03 -0300

    TP4

---
+++ TP4_if.js
@@ -0,0 +1,68 @@
+/*4.	Para el departamento de iluminación:
+Tomando en cuenta que todas las lámparas están en oferta al mismo precio de $35 pesos final.
+A.	Si compra 6 o más  lamparitas bajo consumo tiene un descuento del 50%. 
+B.	Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40 % y si es de otra marca el descuento es del 30%.
+C.	Si compra 4  lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas” se hace un descuento del 25 % y si es de otra marca el descuento es del 20%.
+D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es del 15%, si es  “FelipeLamparas” se hace un descuento del 10 % y si es de otra marca un 5%.
+E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos brutos eh informar del impuesto con el siguiente mensaje:
+ ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
+
+ */
+function CalcularPrecio () 
+{
+    // Declarar variables
+    let vLamp;
+    let vMarca;
+    let vPrecio;
+    let vImp;
+
+    // Tomar valores de campos
+    vLamp=parseInt(document.getElementById("txtIdCantidad").value);
+    vMarca=document.getElementById("Marca").value;
+
+    // Precio estándar de lámparas
+    vPrecio=vLamp*35;
+
+    // Calcular variantes de descuento
+    if (vLamp>=6)
+    {
+        vPrecio=vPrecio*0.5;
+    }
+    else if(vLamp==5 && vMarca=="ArgentinaLuz")
+    {
+        vPrecio=vPrecio*0.6;
+    }    
+    else if (vLamp==5 && vMarca!=="ArgentinaLuz")
+    {        
+        vPrecio=vPrecio*0.7;
+    }
+    else if(vLamp==4 && (vMarca=="ArgentinaLuz" || vMarca=="FelipeLamparas"))
+    {
+        vPrecio=vPrecio*0.75;
+    }
+    else if(vLamp==4 && (vMarca!="ArgentinaLuz" && vMarca!="FelipeLamparas"))
+    {
+        vPrecio=vPrecio*0.8;
+    }
+    else if(vLamp==3 && vMarca=="ArgentinaLuz")
+    {
+        vPrecio=vPrecio*0.85;
+    }
+    else if(vLamp==3 && vMarca=="FelipeLamparas")
+    {
+        vPrecio=vPrecio*0.9;
+    }
+    else if(vLamp==3 && (vMarca!="ArgentinaLuz" && vMarca!="FelipeLamparas"))
+    {
+        vPrecio=vPrecio*0.95;
+    }
+    /// Calcular impuesto y mostrarlo
+    if(vPrecio>120)
+    {
+        vPrecio=vPrecio*1.1
+        vImp=vPrecio*0.1
+        alert("IIBB. Usted pago $"+vImp.toFixed(2));
+    }
+    // Pone el valor del precio en el campo "Precio con descuento"
+    document.getElementById("txtIdprecioDescuento").value=vPrecio.toFixed(2);
+}
